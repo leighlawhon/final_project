d108de55def862fba97093c8ffc7a052703af164
Leigh <leighlawhon@gmail.com>
2017-03-08 12:41:39 -0500

    refactoring for complexity

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,22 +1,24 @@
+function parseResponse(contentType, response) {
+  switch (contentType) {
+    case contentType.includes('application/json'):
+      response.json().catch((error) => { Promise.reject(new Error(`Invalid JSON: ${error.message}`)); });
+      break;
+    case contentType.includes('text/html'):
+      response.text().then(html => ({
+        page_type: 'generic',
+        html,
+      })).catch(error => Promise.reject(new Error(`HTML error: ${error.message}`)));
+      break;
+    default:
+      Promise.reject(new Error(`Invalid content type: ${contentType}`));
+  }
+}
+
 const getURL = function get(url) {
   return fetch(url).then((response) => {
     if (response.ok) {
       const contentType = response.headers.get('Content-Type') || '';
-
-      if (contentType.includes('application/json')) {
-        return response.json().catch((error) => { Promise.reject(new Error(`Invalid JSON: ${error.message}`)); });
-      }
-
-      if (contentType.includes('text/html')) {
-        return response.text().then(html => (
-          {
-            page_type: 'generic',
-            html,
-          }
-        )).catch(error => Promise.reject(new Error(`HTML error: ${error.message}`)));
-      }
-
-      return Promise.reject(new Error(`Invalid content type: ${contentType}`));
+      parseResponse(contentType, response);
     }
 
     if (response.status === 404) {
